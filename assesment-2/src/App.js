eaeb3bc0f979601471083eeec5a5d20d3b081d51
Eli Burgess <eburgess.nexstep@gmail.com>
2021-04-05 15:54:05 -0600

    added a checkbox

---
+++ assesment-2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import CheckBoxes from "./checkboxes"
+function App() {
+  return (
+    <div >
+      <CheckBoxes />
+     
+     
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ assesment-2/src/checkboxes.js
@@ -0,0 +1,20 @@
+const CheckBoxes = (props) => {
+    
+    return (
+        <div>
+            <form>
+                <h1> I panicked  </h1>
+                <label>
+                    user:{""}
+                    <input placeholder="little to late"/>
+                </label>
+                <ol>
+                    <li>1</li>
+                    <ul>2</ul>
+                    <ul>3</ul>
+                </ol>
+            </form>
+        </div>
+    )
+}
+export default CheckBoxes;
